c9984f6c3b319c52b341ccb4ebe8f0010a089559
sqlmerr <107789010+sqlmerr@users.noreply.github.com>
2023-10-27 18:17:04 +0000

    1.0.1 fixes

--- database/models.py
+++ database/models.py
@@ -12,6 +12,7 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(unique=True, nullable=True)
     name: Mapped[str] = mapped_column(nullable=True)
+    email: Mapped[str] = mapped_column(nullable=True)
 
 
 class Desk(Base):
--- main.py
+++ main.py
@@ -1,51 +1,50 @@
 import os
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from fastapi.exceptions import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy import select
 
 from schemas import UserBase, DeskBase
 from database.models import User, Desk
 from config import DB_URL
-# from database.core import engine, sessionmaker
-
-engine = create_async_engine(
-    url=DB_URL
-)
-
-sessionmaker = async_sessionmaker(
-    engine,
-    expire_on_commit=False
-)
+from database.core import engine, sessionmaker
 
 
-app = FastAPI(title="todo api", version="1.0.0")
+app = FastAPI(title="todo api", version="1.0.1")
 
 async def get_session() -> AsyncSession:
-    async with sessionmaker() as session:
-        return session
+    db = sessionmaker()
+    try:
+        yield db
+    finally:
+        await db.close()
 
 @app.get("/")
 async def main():
     return {"message": "Hello!"}
 
 @app.post("/users")
-async def create_user(user: UserBase):
-    session = sessionmaker()
-    u = User(
-        username=user.username,
-        name=user.name
-    )
-    
-    await session.add(u)
-    await session.commit()
-    await session.refresh(u)
-    return u
-    
-    
+async def create_user(user: UserBase, session: AsyncSession = Depends(get_session)):
+    try:
+        u = User(
+            username=user.username,
+            name=user.name
+        )
+        
+        session.add(u)
+        await session.commit()
+        await session.refresh(u)
+        return u
+    except:
+        return HTTPException(409, "This username is already exists")
+
 
 @app.get("/users")
-async def get_users():
-    pass
+async def get_users(session: AsyncSession = Depends(get_session)):
+    results = await session.execute(select(User))
+    usrs = results.scalars().all()
+    return {"users": usrs}
 
 @app.get('users/{username}')
 async def get_user(username: str):
